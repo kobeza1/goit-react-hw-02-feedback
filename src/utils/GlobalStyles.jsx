3c957c03a131ad975bceffb8228946e4c3ae2879
kobeza1 <102040962+kobeza1@users.noreply.github.com>
2022-11-01 19:59:05 +0200

    changes

--- src/utils/GlobalStyles.jsx
+++ src/utils/GlobalStyles.jsx
@@ -1,6 +1,4 @@
 import { Global, css } from '@emotion/react';
-import { theme } from 'constants/theme';
-import { ThemeProvider } from '@emotion/react';
 
 export const GlobalStyles = props => (
   <Global
